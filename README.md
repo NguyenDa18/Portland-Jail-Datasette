f31bb9db8199194ca6cf7c519dc22c59c933200f
Danh Nguyen <danhnguyen267@gmail.com>
2023-04-25 23:36:23 -0700

    Adding charge totals column and increased page limit

--- README.md
+++ README.md
@@ -17,4 +17,10 @@ Creates SQLite database with `inmate_details` and `inmate_charges` tables
 - [Install Datasette](https://docs.datasette.io/en/stable/installation.html#installation)
 
 - Make changes to `csv_to_db.py` file to generate a SQLite .db file that is used to generate a Datasette
+- Run `csv_to_db.py` from the command line
 - Run `datasette pdx_inmates_data.db --open` from the command line
+
+## Deploying
+
+- Run `vercel login` to login to Vercel account
+- Run `datasette publish vercel`
--- csv_to_db.py
+++ csv_to_db.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+import ast
 
 # Read the csv file
 details_csv_file = 'https://raw.githubusercontent.com/NguyenDa18/Portland-Jail-Data-Crawler/main/csvs/inmate_details.csv'
@@ -9,7 +10,15 @@ charges_csv_file = 'https://raw.githubusercontent.com/NguyenDa18/Portland-Jail-D
 details_df = pd.read_csv(details_csv_file)
 
 # Convert the Booking Date column to datetime
-details_df['Booking Date'] = pd.to_datetime(details_df['Booking Date'])
+details_df['Booking Date'] = pd.to_datetime(details_df['Booking Date'], errors='ignore')
+
+# Convert column of Charge Type Counts from dictionary in string to value counts of dictionary
+details_df['Total Charges'] = details_df['Charge Type Counts'].apply(lambda x: ast.literal_eval(x))
+details_df['Total Charges'] = details_df['Total Charges'].apply(lambda x: sum(x.values()))
+details_df = details_df.sort_values(by='Total Charges', ascending=False)
+
+# Remove Charge Type Counts column
+details_df = details_df.drop(columns=['Charge Type Counts'])
 
 # add column names
 column_names = ['Charge', 'Bail Amount', 'Status', 'Name']
